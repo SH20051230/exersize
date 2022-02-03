a2aa7d33c6106b32648dbbf7ae3b3fe814bd3642
SH20051230 <98988694+SH20051230@users.noreply.github.com>
2022-02-04 11:43:20 +1300

    Create exersize 1.py

---
+++ exersize 1.py
@@ -0,0 +1,6 @@
+num1 = int(input("what is the first number"))
+num2 = int(input("what is the second number"))
+if num1 > num2:
+    print(f"The biggest number is {num1}")
+else:
+    print(f"The biggest number is {num2}")
