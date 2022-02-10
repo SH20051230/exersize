ecb2e079562c941d2596ad6871bbadfd29633c97
SH20051230 <98988694+SH20051230@users.noreply.github.com>
2022-02-11 11:12:47 +1300

    Create exersize 4.py

---
+++ exersize 4.py
@@ -0,0 +1,13 @@
+name = []
+mark = []
+
+input_name = str(input("what is the student's name: "))
+name.append(input_name)
+input_mark = int(input("what is the student's mark: "))
+mark.append(input_mark)
+while name != "x":
+    name = str(input("what is the student's name: "))
+    mark = int(input("what is the student's mark: "))
+
+
+
