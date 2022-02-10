5f75699741276d57f29747d41000e35cf471b0ec
SH20051230 <98988694+SH20051230@users.noreply.github.com>
2022-02-11 11:12:43 +1300

    Create exersize 3.py

---
+++ exersize 3.py
@@ -0,0 +1,9 @@
+Commercial_= 0.5m
+Resisdential_= Commercial_thickness/2
+building_type=int(input("what is the type: "))
+if Commercial_
+
+length= int(input("what is the length: "))
+width= int(input("what is the width： "))
+
+
